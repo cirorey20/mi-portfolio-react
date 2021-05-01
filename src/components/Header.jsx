cd47e38fa97f93bddaf9d8973389f882409b5020
cirorey20 <cirorey95@gmail.com>
2021-05-01 17:18:17 -0300

    elimine el backfground center del header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
 
         <Fragment>
             <div className="container-fluid img-header">
-                <div className="col text-center">
+                <div>
                     <h1 className="text-tamaño titulooo">CIRO REY</h1>
                     <h3 className="text-center subtitle">Desarrollador Web</h3>
                 </div>
